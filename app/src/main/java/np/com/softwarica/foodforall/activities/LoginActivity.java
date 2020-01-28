bbfab0021dd8d345ae20298f8f5914277c7d053e
Shuvakamana Chaudhary <shuvakamanacdy@gmail.com>
2020-01-28 14:00:00 +0100

    login is done

--- app/src/main/java/np/com/softwarica/foodforall/activities/LoginActivity.java
+++ app/src/main/java/np/com/softwarica/foodforall/activities/LoginActivity.java
@@ -1,16 +1,163 @@
 package np.com.softwarica.foodforall.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.app.Notification;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
 
 import np.com.softwarica.foodforall.R;
+import np.com.softwarica.foodforall.utils.CreateChannel;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    protected EditText etEmail, etPassword;
+    private Button btnLogin;
+    private FirebaseAuth auth;
+    private SensorManager manager;
+    private Sensor sensor;
+    private SensorEventListener event;private int id = 1;
+    CreateChannel createChannel;
+    private NotificationManagerCompat notificationManagerCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+
+        btnLogin.setOnClickListener(view -> loginUser());
+
+        manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = manager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        event = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if (sensorEvent.values[0] <= 0) {
+                    loginUser();
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
+        checkPermission();
+
+        createChannel = new CreateChannel(this);
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+        createChannel.createChannel();
+    }
+
+    private void checkPermission() {
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ).withListener(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport report) {
+                if (report.isAnyPermissionPermanentlyDenied()) {
+                    checkPermission();
+                }
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {/* ... */}
+        }).check();
+    }
+
+    private void loginUser() {
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if (email.equals("")) {
+            etEmail.setError("Email is required!!!");
+            etEmail.requestFocus();
+            return;
+        } else if (password.equals("")) {
+            etPassword.setError("Password is required!!!");
+            etPassword.requestFocus();
+            return;
+        }
+
+        ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setMessage("Loading...");
+        dialog.setCancelable(false);
+        dialog.show();
+
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            dialog.dismiss();
+            if (task.isSuccessful()) {
+                showLoginNotification();
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            } else {
+                Toast.makeText(this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void openRegister(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        manager.registerListener(event, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        manager.unregisterListener(event);
+    }
+
+    private void showLoginNotification() {
+        Notification notification = new NotificationCompat.Builder(this, CreateChannel.CHANNEL_1)
+                .setSmallIcon(R.drawable.logo)
+                .setContentTitle("User Log In")
+                .setContentText("You have successfully logged in!!!")
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        notificationManagerCompat.notify(id, notification);
+        id++;
     }
 }
